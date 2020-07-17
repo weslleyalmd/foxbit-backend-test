77b9ce30211c31eac38724db88e4f11c4536d940
Weslley Almeida <weslley@weslley.almd>
2020-07-17 01:35:12 -0300

    Move hovers according to instructions

--- lib/hover.rb
+++ lib/hover.rb
@@ -1,19 +1,57 @@
+require 'byebug'
+
 class Hover
   attr_accessor :x_coord, :y_coord, :direction
 
+  DIRECTIONS = {
+    N: {axis_to_move: "y", R: "E", L: "W", type: "add"},
+    S: {axis_to_move: "y", R: "W", L: "E", type: "subtract"},
+    E: {axis_to_move: "x", R: "S", L: "N", type: "add"},
+    W: {axis_to_move: "x", R: "N", L: "S", type: "subtract"},
+  }
+
   def initialize(x_coord, y_coord, direction)
-    @x_coord = x_coord
-    @y_coord = y_coord
+    @x_coord = x_coord.to_i
+    @y_coord = y_coord.to_i
     @direction = direction
   end
 
   def current_position
-    "#{x_coord} #{y_coord} #{direction}"
+    "#{@x_coord} #{@y_coord} #{@direction}\n"
   end
 
   def move(instructions)
-    instructions.each_char do |i|
+    instructions.each_char do |instruction|
+
+      if instruction == "L" || instruction == "R"
+        # GET new direction based on current direction (@direction attr)
+        @direction = DIRECTIONS[@direction.to_sym][instruction.to_sym]
+      end
+
+      if instruction == "M"
+
+        type = DIRECTIONS[@direction.to_sym][:type]
+        axis_to_move = DIRECTIONS[@direction.to_sym][:axis_to_move]
+        
+        if type == "add"
+          if axis_to_move == "x"
+            @x_coord += 1
+          elsif axis_to_move == "y"
+            @y_coord += 1
+          end              
+        end
+
+        if type == "subtract"
+          if axis_to_move == "x"
+            @x_coord -= 1
+          elsif axis_to_move == "y"
+            @y_coord -= 1
+          end
+        end
+
+      end
+
     end
-  end 
+  end
 
 end
--- lib/processor.rb
+++ lib/processor.rb
@@ -8,16 +8,37 @@ class Processor
 
   def initialize(file_path)
     @file = read(file_path)
+    @plateau = nil
+    @hovers = []
   end
 
   def run
     final_boundary_x, final_boundary_y = file[0].split(" ")
     @plateau = Plateau.new(final_boundary_x, final_boundary_y)
     
-    byebug
+    # remove plateau boundary coordinates
+    @file.delete_at(0)
+
+    # process instructions
+    move_hovers(@file)
+
+    print_hovers_positions
   end
 
   private
+
+  def print_hovers_positions
+    @hovers.map {|h| h.current_position}
+  end
+
+  def move_hovers(hovers)
+    hovers.each_slice(2) do |initial_position, instructions|
+      x_coord, y_coord, direction = initial_position.split(" ")
+      hover = Hover.new(x_coord, y_coord, direction)
+      hover.move(instructions)
+      @hovers << hover
+    end
+  end
 
   def read(file_path)
     if File.exists? file_path
